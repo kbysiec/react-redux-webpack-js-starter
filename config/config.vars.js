8e6025722345aa090400535dd6077adc10900f95
kbysiec <kamil.bysiec.agileplayers@gmail.com>
2018-05-02 15:45:21 +0200

    added project config vars and paths

--- config/config.vars.js
+++ config/config.vars.js
@@ -1,7 +1,38 @@
-const path = require('path');
+const path = require("path");
 
-const PATHS = {
-    x: 1,
+const projectConfigPaths = {
+    root: path.resolve(__dirname, ".."),
+    src: path.resolve(__dirname, "../src"),
+    dist: path.resolve(__dirname, "../dist"),
 };
 
-export default PATHS;
+const projectConfigVars = {};
+
+const configure = function(env) {
+    const useTs = env.ts == "true";
+
+    Object.assign(projectConfigPaths, {
+        appEntry: useTs ? path.resolve(__dirname, "../src/index.tsx")
+            : path.resolve(__dirname, "../src/index.js"),
+    });
+
+    Object.assign(projectConfigVars, {
+        isDev: env.dev == "true",
+        isTest: env.test == "true",
+        isProd: env.prod == "true",
+        useTs: useTs,
+        useSourceMaps: env.sourceMaps == "true",
+        useAwesomeLoader: env.awesome == "true",
+    });
+
+    return {
+        PATHS: projectConfigPaths,
+        VARS: projectConfigVars,
+    }
+};
+
+module.exports = {
+    PATHS: projectConfigPaths,
+    VARS: projectConfigVars,
+    configure: configure,
+};
