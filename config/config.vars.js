88db12a04239c4dac52a3c09c91eba52891af98e
kbysiec <kamil.bysiec.agileplayers@gmail.com>
2018-05-11 22:25:55 +0200

    removed ts project env variable

--- config/config.vars.js
+++ config/config.vars.js
@@ -9,18 +9,10 @@ const projectConfigPaths = {
 const projectConfigVars = {};
 
 const configure = function(env) {
-    const useTs = env.ts == "true";
-
-    Object.assign(projectConfigPaths, {
-        appEntry: useTs ? path.resolve(__dirname, "../src/index.tsx")
-            : path.resolve(__dirname, "../src/index.js"),
-    });
-
     Object.assign(projectConfigVars, {
         isDev: env.dev == "true",
         isTest: env.test == "true",
         isProd: env.prod == "true",
-        useTs: useTs,
         useSourceMaps: env.sourceMaps == "true",
         useAwesomeLoader: env.awesome == "true",
     });
