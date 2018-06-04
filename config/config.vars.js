e41fe0dfa6c7e14773f505368ec8d56e52a83456
kbysiec <kamil.bysiec.agileplayers@gmail.com>
2018-06-04 21:30:17 +0200

    remove env variable

--- config/config.vars.js
+++ config/config.vars.js
@@ -1,41 +1,43 @@
-const path = require("path");
+const path = require('path');
 
 const projectConfigPaths = {
-    root: path.resolve(__dirname, ".."),
-    src: path.resolve(__dirname, "../src"),
-    dist: path.resolve(__dirname, "../dist"),
+  root: path.resolve(__dirname, '..'),
+  src: path.resolve(__dirname, '../src'),
+  dist: path.resolve(__dirname, '../dist'),
 };
 
 const projectConfigVars = {};
 const projectConfigVarsDefaults = {
-    isDev: true,
-    isProd: false,
-    useBabelPolyfill: "entry",
-    useSourceMaps: true,
-    useAwesomeLoader: false,
-    supportedBrowsers: [">0.25%"],
+  useBabelPolyfill: 'entry',
+  useSourceMaps: true,
+  useAwesomeLoader: false,
+  supportedBrowsers: ['>0.25%'],
 };
 
-const configure = function(env) {
-    Object.assign(projectConfigVars, {
-        isDev: env.dev ? env.dev == "true" : projectConfigVarsDefaults.isDev,
-        isProd: env.prod ? env.prod == "true" : projectConfigVarsDefaults.isProd,
-        useBabelPolyfill: env.babelPolyfill ? env.babelPolyfill : projectConfigVarsDefaults.useBabelPolyfill,
-        useSourceMaps: env.sourceMaps ? env.sourceMaps == "true" : projectConfigVarsDefaults.useSourceMaps,
-        useAwesomeLoader: env.awesomeLoader ? env.awesomeLoader == "true" : projectConfigVarsDefaults.useAwesomeLoader,
-        supportedBrowsers: env.supportedBrobabelPolyfillwsers ?
-            env.supportedBrowsers.split(",").map(s => s.trim()) :
-            projectConfigVarsDefaults.supportedBrowsers,
-    });
-    
-    return {
-        PATHS: projectConfigPaths,
-        VARS: projectConfigVars,
-    }
+const configure = function (env) {
+  Object.assign(projectConfigVars, {
+    useBabelPolyfill: env.babelPolyfill
+      ? env.babelPolyfill
+      : projectConfigVarsDefaults.useBabelPolyfill,
+    useSourceMaps: env.sourceMaps
+      ? env.sourceMaps == 'true'
+      : projectConfigVarsDefaults.useSourceMaps,
+    useAwesomeLoader: env.awesomeLoader
+      ? env.awesomeLoader == 'true'
+      : projectConfigVarsDefaults.useAwesomeLoader,
+    supportedBrowsers: env.supportedBrobabelPolyfillwsers
+      ? env.supportedBrowsers.split(',').map(s => s.trim())
+      : projectConfigVarsDefaults.supportedBrowsers,
+  });
+
+  return {
+    PATHS: projectConfigPaths,
+    VARS: projectConfigVars,
+  };
 };
 
 module.exports = {
-    PATHS: projectConfigPaths,
-    VARS: projectConfigVars,
-    configure: configure,
+  PATHS: projectConfigPaths,
+  VARS: projectConfigVars,
+  configure,
 };
