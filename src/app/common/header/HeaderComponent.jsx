eedaed2741051d36124e763c1b38de636950e27f
kbysiec <kamil.bysiec.agileplayers@gmail.com>
2018-07-13 15:32:40 +0200

    added HeaderComponent with tests

---
+++ src/app/common/header/HeaderComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavbarComponent from './components/navbar/NavbarComponent';
+import './header.scss';
+import logo from './_assets/img/react.svg';
+
+const HeaderComponent = () => (
+  <div className="header-wrapper">
+    <div className="header">
+      <img src={logo} className="header-logo" alt="logo" />
+      <NavbarComponent />
+    </div>
+  </div>
+);
+
+export default HeaderComponent;
