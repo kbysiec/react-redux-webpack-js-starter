548b0f05f1ef6311513fa165657fa3f4c5cc6bd2
kbysiec <kamil.bysiec.agileplayers@gmail.com>
2018-07-14 19:12:55 +0200

    added helper sample function

--- src/helpers/__tests__/example.test.js
+++ src/helpers/__tests__/example.test.js
@@ -1,5 +1,9 @@
-import { sum } from '../../helpers/exampleHelpers';
+import { sum, subtract } from '../../helpers/exampleHelpers';
 
-it('adds 1 + 2 to equal 3', () => {
+it('should add 1 + 2', () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+it('should subtract 2 from 3', () => {
+  expect(subtract(3, 2)).toBe(1);
 });
