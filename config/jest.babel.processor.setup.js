f938140290ba19e09d784b29a8f047ed99f6ca74
Kamil Bysiec <kamil.bysiec.agileplayers@gmail.com>
2019-03-28 21:24:53 +0100

    added dynamic import support to jest unit testing

--- config/jest.babel.processor.setup.js
+++ config/jest.babel.processor.setup.js
@@ -1,21 +1,15 @@
 const babel = require('@babel/core');
 const jestPreset = require('babel-preset-jest');
+const dynamicNodePlugin = require('babel-plugin-dynamic-import-node');
 
 module.exports = {
   process(src, filename) {
-    // const transformCfg = {
-    //   presets: ['@babel/env', '@babel/react', jestPreset],
-    //   plugins: ["@babel/plugin-transform-runtime", istanbul],
-    //   retainLines: true,
-    // };
-    // return babel.transform(src, transformCfg).code;
-
     return babel
       .transform(src, {
         // auxiliaryCommentBefore: ' istanbul ignore next ',
         filename,
         presets: ['@babel/env', '@babel/react', jestPreset],
-        plugins: ['@babel/plugin-transform-runtime'],
+        plugins: ['@babel/plugin-transform-runtime', dynamicNodePlugin],
         retainLines: true,
       })
       .code.replace(
